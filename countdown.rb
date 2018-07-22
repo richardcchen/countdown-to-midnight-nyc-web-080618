b85874a5abf845a44b67614d6126fd49d801872f
Richard Chen <richard.c.chen@gmail.com>
2018-07-22 21:07:05 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,12 +10,3 @@ def countdown (int)
 "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep(int)
-  counter = int
-  while counter > 0
-    puts "#{counter} SECOND(S)!"
-    counter -= 1
-    sleep(1)
-  end
-"HAPPY NEW YEAR!"  
-end
