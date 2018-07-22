19b5c65ec132f0641ca35ab73081c4b9272b36e6
Richard Chen <richard.c.chen@gmail.com>
2018-07-22 21:09:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown (int)
+  counter = int
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+#puts "HAPPY NEW YEAR!"
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  counter = int
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep(1)
+  end
+#puts "HAPPY NEW YEAR!"
+"HAPPY NEW YEAR!"
 end
